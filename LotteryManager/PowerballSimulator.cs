289a42a711185f31bccc272e1e9f54ca73d7065a
bassman00 <github@dascensio.com>
2017-07-19 12:02:20 -0400

    Remove code analyzer files

--- LotteryManager/PowerballSimulator.cs
+++ LotteryManager/PowerballSimulator.cs
@@ -47,33 +47,33 @@ namespace LotteryManager
         private readonly Dictionary<string, long> WinGrid = new Dictionary<string, long>();
         private readonly Dictionary<string, double> PrizeList = new Dictionary<string, double>();
         private readonly Dictionary<int, long> WhiteNumberFrequency = new Dictionary<int, long>();
-		private readonly Dictionary<int, long> RedNumberFrequency = new Dictionary<int, long>();
+        private readonly Dictionary<int, long> RedNumberFrequency = new Dictionary<int, long>();
 
         private Random rand;
 
-		public List<Ticket> PlayerTicketList = new List<Ticket>();
+        public List<Ticket> PlayerTicketList = new List<Ticket>();
 
         public long NumberOfDraws { get; set; }
         public long DisplayFrequency { get; set; }
 
         public PowerballSimulator(long draws, long displayFrequency)
         {
-			rand = new Random();
-			NumberOfDraws = draws;
+            rand = new Random();
+            NumberOfDraws = draws;
             DisplayFrequency = draws > 0 && displayFrequency > draws ? draws : displayFrequency;
 
             PrizeList.Add(totalDraws, 0);
-			PrizeList.Add(totalPlays, -2.0);
-			PrizeList.Add(loser, 0.0);
-			PrizeList.Add(pbOnly, 4.0);
-			PrizeList.Add(pbPlusOne, 4.0);
-			PrizeList.Add(pbPlusTwo, 7.0);
-			PrizeList.Add(threeWhite, 7.0);
-			PrizeList.Add(pbPlusThree, 100.0);
-			PrizeList.Add(fourWhite, 100.0);
-			PrizeList.Add(pbPlusFour, 50000.0);
-			PrizeList.Add(fiveWhite, 1000000.00);
-			PrizeList.Add(jackpot, 20000000.00);
+            PrizeList.Add(totalPlays, -2.0);
+            PrizeList.Add(loser, 0.0);
+            PrizeList.Add(pbOnly, 4.0);
+            PrizeList.Add(pbPlusOne, 4.0);
+            PrizeList.Add(pbPlusTwo, 7.0);
+            PrizeList.Add(threeWhite, 7.0);
+            PrizeList.Add(pbPlusThree, 100.0);
+            PrizeList.Add(fourWhite, 100.0);
+            PrizeList.Add(pbPlusFour, 50000.0);
+            PrizeList.Add(fiveWhite, 1000000.00);
+            PrizeList.Add(jackpot, 20000000.00);
 
             foreach (string stat in statKeys)
                 WinGrid.Add(stat, 0);
@@ -86,7 +86,7 @@ namespace LotteryManager
             {
                 RedNumberFrequency.Add(i, 0);
             }
-		}
+        }
 
         public void LoadTickets(string fpath)
         {
@@ -117,23 +117,23 @@ namespace LotteryManager
 
         public void Run()
         {
-			var Ticket = new PowerballTicket(DateTime.Today);
+            var Ticket = new PowerballTicket(DateTime.Today);
 
-			string input = "";
+            string input = "";
 
             while (input.ToUpper() != "Q")
-			{
-				//Console.WriteLine(PlayerTicket.Show());
-				if (NumberOfDraws > 0 && WinGrid[totalDraws] == NumberOfDraws)
-					break;
+            {
+                //Console.WriteLine(PlayerTicket.Show());
+                if (NumberOfDraws > 0 && WinGrid[totalDraws] == NumberOfDraws)
+                    break;
 
-				WinGrid[totalDraws]++;
-				QuickPick(rand, Ticket);
+                WinGrid[totalDraws]++;
+                QuickPick(rand, Ticket);
                 foreach (Ticket PlayerTicket in PlayerTicketList)
                 {
-					CompareTickets(PlayerTicket, Ticket);
+                    CompareTickets(PlayerTicket, Ticket);
                 }
-				if (WinGrid[totalPlays] % DisplayFrequency == 0)
+                if (WinGrid[totalPlays] % DisplayFrequency == 0)
                 {
                     DisplayScreen();
                     if(Console.KeyAvailable)
@@ -152,22 +152,22 @@ namespace LotteryManager
                         }
                     }
                 }
-			}
+            }
             DisplayScreen();
-		}
+        }
 
-		public void QuickPick(Random rand, Ticket ticket)
-		{
+        public void QuickPick(Random rand, Ticket ticket)
+        {
             ticket.Clear();
-			for (int i = 0; i < PowerballLimits.WhiteBallCount; i++)
-			{
+            for (int i = 0; i < PowerballLimits.WhiteBallCount; i++)
+            {
                 int whiteBall = rand.Next(PowerballLimits.WhiteBallMin, PowerballLimits.WhiteBallMax + 1);
                 while (!ticket.AddWhiteBall(whiteBall))
                 {
                     whiteBall = rand.Next(PowerballLimits.WhiteBallMin, PowerballLimits.WhiteBallMax + 1);
                 }
-				WhiteNumberFrequency[whiteBall]++;
-			}
+                WhiteNumberFrequency[whiteBall]++;
+            }
             for (int i = 0; i < PowerballLimits.RedBallCount; i++)
             {
                 int redBall = rand.Next(PowerballLimits.RedBallMin, PowerballLimits.RedBallMax + 1);
@@ -175,26 +175,26 @@ namespace LotteryManager
                 {
                     redBall = rand.Next(PowerballLimits.RedBallMin, PowerballLimits.RedBallMax + 1);
                 }
-				RedNumberFrequency[redBall]++;
-			}
-   		}
+                RedNumberFrequency[redBall]++;
+            }
+        }
 
-		public string CompareTickets(Ticket t1, Ticket t2)
-		{
-			bool winner = false;
-			int whiteMatch = 0;
-			int redMatch = 0;
+        public string CompareTickets(Ticket t1, Ticket t2)
+        {
+            bool winner = false;
+            int whiteMatch = 0;
+            int redMatch = 0;
 
-			WinGrid[totalPlays]++; //=PlayerTicketList.Count;
+            WinGrid[totalPlays]++; //=PlayerTicketList.Count;
 
             foreach (int number in t1.GetWhiteBalls())
-			{
-				if (t2.GetWhiteBalls().Contains(number))
-				{
-					winner = true;
-					whiteMatch++;
-				}
-			}
+            {
+                if (t2.GetWhiteBalls().Contains(number))
+                {
+                    winner = true;
+                    whiteMatch++;
+                }
+            }
             foreach (int number in t1.GetRedBalls())
             {
                 if (t2.GetRedBalls().Contains(number))
@@ -203,100 +203,100 @@ namespace LotteryManager
                     winner = true;
                 }
             }
-			string results = "";
+            string results = "";
 
-			if (winner)
-			{
-				if (redMatch > 0)
-				{
-					switch (whiteMatch)
-					{
-						case 0:
-							WinGrid[pbOnly]++;
-							break;
+            if (winner)
+            {
+                if (redMatch > 0)
+                {
+                    switch (whiteMatch)
+                    {
+                        case 0:
+                            WinGrid[pbOnly]++;
+                            break;
 
-						case 1:
-							WinGrid[pbPlusOne]++;
-							break;
+                        case 1:
+                            WinGrid[pbPlusOne]++;
+                            break;
 
-						case 2:
-							WinGrid[pbPlusTwo]++;
-							break;
+                        case 2:
+                            WinGrid[pbPlusTwo]++;
+                            break;
 
-						case 3:
-							WinGrid[pbPlusThree]++;
-							break;
+                        case 3:
+                            WinGrid[pbPlusThree]++;
+                            break;
 
-						case 4:
-							WinGrid[pbPlusFour]++;
-							break;
+                        case 4:
+                            WinGrid[pbPlusFour]++;
+                            break;
 
-						case 5:
-							WinGrid[jackpot]++;
-							break;
+                        case 5:
+                            WinGrid[jackpot]++;
+                            break;
 
-					};
-				}
-				else
-				{
-					switch (whiteMatch)
-					{
-						case 3:
-							WinGrid[threeWhite]++;
-							break;
+                    };
+                }
+                else
+                {
+                    switch (whiteMatch)
+                    {
+                        case 3:
+                            WinGrid[threeWhite]++;
+                            break;
 
-						case 4:
-							WinGrid[fourWhite]++;
-							break;
+                        case 4:
+                            WinGrid[fourWhite]++;
+                            break;
 
-						case 5:
-							WinGrid[fiveWhite]++;
-							break;
+                        case 5:
+                            WinGrid[fiveWhite]++;
+                            break;
 
                         default:
                             WinGrid[loser]++;
                             break;
-					};
-				}
-				results = String.Format("Winner!  Matched {0} white and {1} red", whiteMatch, redMatch);
-			}
-			else
+                    };
+                }
+                results = String.Format("Winner!  Matched {0} white and {1} red", whiteMatch, redMatch);
+            }
+            else
             {
-				WinGrid[loser]++;
-				results = "Loser!";
-			}
+                WinGrid[loser]++;
+                results = "Loser!";
+            }
 
-			return results;
-		}
+            return results;
+        }
 
-		private void DisplayWins()
-		{
-			string output = "";
-			double netWinnings = 0;
-			foreach (KeyValuePair<string, long> entry in WinGrid)
-			{
-				double dollars = entry.Value * PrizeList[entry.Key];
-				netWinnings += dollars;
-				string line = String.Format("{0} - {1,15:n0} - {2,20:c0}\n",
-											entry.Key, entry.Value, dollars);
-				output += line;
-			}
-			Console.WriteLine(output);
+        private void DisplayWins()
+        {
+            string output = "";
+            double netWinnings = 0;
+            foreach (KeyValuePair<string, long> entry in WinGrid)
+            {
+                double dollars = entry.Value * PrizeList[entry.Key];
+                netWinnings += dollars;
+                string line = String.Format("{0} - {1,15:n0} - {2,20:c0}\n",
+                                            entry.Key, entry.Value, dollars);
+                output += line;
+            }
+            Console.WriteLine(output);
             string totalLine = String.Format("Total 'Winnings': {0,20:c0}\nTotal Net: {1,28:c0}",
-											 netWinnings + (WinGrid[totalPlays] * -PrizeList[totalPlays]),
-											 netWinnings);
-			Console.WriteLine(totalLine);
-		}
+                                             netWinnings + (WinGrid[totalPlays] * -PrizeList[totalPlays]),
+                                             netWinnings);
+            Console.WriteLine(totalLine);
+        }
 
         public void DisplayScreen()
         {
-			Console.Clear();
-			foreach (Ticket PlayerTicket in PlayerTicketList)
-				Console.WriteLine(PlayerTicket.Show());
+            Console.Clear();
+            foreach (Ticket PlayerTicket in PlayerTicketList)
+                Console.WriteLine(PlayerTicket.Show());
 
-			Console.WriteLine();
-			DisplayWins();
-		}
+            Console.WriteLine();
+            DisplayWins();
+        }
 
-	}
+    }
 }
